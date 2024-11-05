9acedf10a41e02f5bfa6df81258b2b9fcffabdc8
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2024-11-05 14:33:44 -0300

    02.Manipulando elementos da página

--- script.js
+++ script.js
@@ -2,13 +2,45 @@ const html = document.querySelector('html')
 const Foco_bt = document.querySelector('.app__card-button--foco')
 const ShortBreak_bt = document.querySelector('.app__card-button--curto')
 const LongBreak_bt = document.querySelector('.app__card-button--longo')
+const img = document.querySelector('.app__image')
+const title = document.querySelector('.app__title')
 
 Foco_bt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'foco')
+    altContext('foco')
 })
 ShortBreak_bt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-curto')
+    altContext('descanso-curto')
 })
 LongBreak_bt.addEventListener('click', () => {
-    html.setAttribute('data-contexto', 'descanso-longo')
+    altContext('descanso-longo')
 })
+function altContext(context){
+    html.setAttribute('data-contexto', context)
+    img.setAttribute('src', `./imagens/${context}.png`)
+    switch (context) {
+        case "foco":
+            title.innerHTML =
+            `
+            Otimize sua produtividade,<br>
+            <strong class="app__title-strong">mergulhe no que importa.</strong>
+            `
+            break;
+        case "descanso-curto":
+            title.innerHTML =
+            `
+            Que tal dar uma respirada?<br>
+            <strong class="app__title-strong">Faça uma pausa curta!</strong>
+            `
+            break;
+        case "descanso-longo":{
+            title.innerHTML =
+            `
+            Hora de voltar à superficie<br>
+            <strong class="app__title-strong">Faça uma pausa longa.</strong>
+            `
+            break;
+        }
+        default:
+            break;
+    }
+}
