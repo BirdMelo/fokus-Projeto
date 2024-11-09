8e74a848a59653d355a7d49f74d669db8c5699d4
BirdMelo <jpmn2013.terra.com.br@gmail.com>
2024-11-09 11:27:43 -0300

    05.Finalizando o projeto

--- app/script.js
+++ app/script.js
@@ -1,25 +1,31 @@
 import { altContext } from './altContext.js';
 import { song } from './song.js';
-import { start } from './timer.js';
+import { showTimer, start } from './timer.js';
 
 const Foco_bt = document.querySelector('.app__card-button--foco')
 const ShortBreak_bt = document.querySelector('.app__card-button--curto')
 const LongBreak_bt = document.querySelector('.app__card-button--longo')
 export const toggleTimer = document.querySelector('#start-pause')
 const toggleSong = document.querySelector('#alternar-musica')
-export const breakTime = {value: 1500}
+export let breakTime = 1500
+export function setBreakTime(value){
+    breakTime = value
+    showTimer()
+}
 
 Foco_bt.addEventListener('click', () => {
-    breakTime.value = 1500
+    setBreakTime(1500)
     altContext('foco')
 })
 ShortBreak_bt.addEventListener('click', () => {
-    breakTime.value = 300
+    setBreakTime(300)
     altContext('descanso-curto')
 })
 LongBreak_bt.addEventListener('click', () => {
-    breakTime.value = 900
+    setBreakTime(900)
     altContext('descanso-longo')
 })
 toggleSong.addEventListener('change', song)
 toggleTimer.addEventListener('click', start)
+
+showTimer()
--- app/timer.js
+++ app/timer.js
@@ -1,10 +1,11 @@
-import { toggleTimer, breakTime } from "./script.js"
+import { toggleTimer, breakTime, setBreakTime } from "./script.js"
 
 const playSong = new Audio('../sons/play.wav')
 const pauseSong = new Audio('../sons/pause.mp3')
 const timeoutSong = new Audio('../sons/beep.mp3')
 const timer = document.querySelector('#timer')
 let interval = null
+
 export function start() {
     if (interval) {
         pauseSong.play();
@@ -15,7 +16,6 @@ export function start() {
     playSong.play();
     toggleButton('Pause', '../imagens/pause.png')
     interval = setInterval(countdown, 1000);
-    showTimer()
 }
 
 function toggleButton(toggle, img){
@@ -23,22 +23,21 @@ function toggleButton(toggle, img){
     toggleTimer.querySelector('img').setAttribute('src', img)
 }
 const countdown = () => {
-    if (breakTime.value <= 0) {
+    if (breakTime <= 0) {
         // timeoutSong.play();
         clearTime();
-        breakTime.value = 5;
+        setBreakTime(5)
         toggleButton('Começar', '../imagens/play_arrow.png')
         return;
     }
-    breakTime.value -= 1;
-    showTimer()
+    setBreakTime(breakTime - 1)
     // console.log(`temporizador: ${breakTime}`);
 };
 function clearTime() {
     clearInterval(interval);
     interval = null;
 }
-function showTimer(){
-    const time = new Date(breakTime.value * 1000).toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
+export function showTimer(){
+    const time = new Date(breakTime * 1000).toLocaleTimeString('pt-Br', {minute: '2-digit', second: '2-digit'})
     timer.innerHTML = `${time}`
 }
